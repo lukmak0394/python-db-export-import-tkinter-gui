efcf10e2eaf654ae455e4ae91f38eeea573e7c93
lukmak0394 <=>
2023-09-10 10:48:36 +0200

    reorganised project structure

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import classes.database as db
+
+def main():
+    db_instance = db.Database()
+    tables = db_instance.get_db_tables()
+    print(tables)
+
+
+main()
+    
+
+
+
