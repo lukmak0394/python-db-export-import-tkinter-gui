c819773873eccfd32f9f6f1281595b5b6ae41174
lukmak0394 <=>
2023-09-15 16:35:28 +0200

    simple save conditions done - needs improvements

--- Export.py
+++ Export.py
@@ -1,19 +1,19 @@
+import os
+from dotenv import load_dotenv
 import Database as db
 import SilentErrorHandler as erh
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 import pandas as pd
 import datetime
-import os
-from dotenv import load_dotenv
 import sqlalchemy.exc as sqe
 
 class Export:
 
     __conn = None
 
-    __tk_window = None
-    __tk_window_title = ""
+    __tk_root_window = None
+    __tk_root_window_title = ""
 
     __columns_listbox = None
     __tables_listbox = None
@@ -30,7 +30,8 @@ class Export:
         2:"CSV",
     }
 
-    __condition_rows_counter = 2
+    __condition_rows_counter = 3
+    __query_conditions = []
 
     def __init__(self,tables,connection):
         if not connection:
@@ -41,22 +42,22 @@ class Export:
         
         load_dotenv()
         self.__conn = connection
-        self.__tk_window_title = "Export data from database"
-        self.__tk_window = tk.Tk()
+        self.__tk_root_window_title = "Export data from database"
+        self.__tk_root_window = Tk()
         self.__db_tables = tables
         self.__export_folder = os.getenv("export_folder")
 
 
     def open_window(self):
-        if not self.__tk_window or not self.__conn:
+        if not self.__tk_root_window or not self.__conn:
             return False
         
-        win = self.__tk_window
-        win.title(self.__tk_window_title)
+        win = self.__tk_root_window
+        win.title(self.__tk_root_window_title)
         win.geometry("800x300")
 
-        self.__tables_listbox = tk.Listbox(win, selectmode=tk.SINGLE)
-        self.__columns_listbox = tk.Listbox(win, selectmode=tk.MULTIPLE)
+        self.__tables_listbox = Listbox(win, selectmode=SINGLE)
+        self.__columns_listbox = Listbox(win, selectmode=MULTIPLE)
         self.__tables_listbox.grid(row=0, column=0, sticky="nsew")
         self.__columns_listbox.grid(row=0, column=1, sticky="nsew")
 
@@ -73,11 +74,11 @@ class Export:
         
         data = self.__db_tables
         if len(data):
-            window = self.__tk_window
+            window = self.__tk_root_window
             listbox = self.__tables_listbox
 
             for item in data:
-                listbox.insert(tk.END, item)
+                listbox.insert(END, item)
 
             def save_selection():
                 self.__selected_table = ""
@@ -87,7 +88,7 @@ class Export:
                 print(f"{date} - selected table: {self.__selected_table}")
                 self.__insert_column_names_to_listbox()
                 
-            save_button = tk.Button(window, text="Save table", command=save_selection)
+            save_button = Button(window, text="Save table", command=save_selection)
             save_button.grid(row=2, column=0,sticky="nsew")
         
         return True
@@ -111,12 +112,12 @@ class Export:
             return False
        
         if len(columns):
-            window = self.__tk_window
+            window = self.__tk_root_window
             listbox = self.__columns_listbox
 
-            listbox.delete(0, tk.END)
+            listbox.delete(0, END)
             for column in columns:
-                listbox.insert(tk.END, column)
+                listbox.insert(END, column)
 
             def save_selection():
                 self.__selected_columns.clear()
@@ -126,80 +127,86 @@ class Export:
                 self.__display_export_buttons()
                 self.__open_conditions_window(columns)
 
-            save_button = tk.Button(window, text="Select columns", command=save_selection)
+            save_button = Button(window, text="Select columns", command=save_selection)
             save_button.grid(row=2,column=1,sticky="nsew")
         
         return True
 
     def __display_export_buttons(self):
-        window = self.__tk_window
+        window = self.__tk_root_window
         export_formats = self.__export_formats
         i = 0
         for key in export_formats:
             btn_txt = f"Export {export_formats[key]}"
-            export_format_btn = tk.Button(window,text=btn_txt, command=lambda m=key: self.__export(m), bg="#0d6efd", fg="white")
+            export_format_btn = Button(window,text=btn_txt, command=lambda m=key: self.__export(m), bg="#0d6efd", fg="white")
             export_format_btn.grid(row=3,column=i, sticky="nsew")
             i+=1
 
     def __open_conditions_window(self,cols):
+        top = Toplevel()
+        top.title("Set conditions")
+        top.geometry("500x200")
 
-        window = tk.Tk()
+        btn_add_row = Button(top, text="Add next", command=lambda m=top, c=cols: self.__add_conditions_row(m,c), bg="#0d6efd", fg="white")
+        btn_add_row.grid(row=1, column=1, columnspan=4, sticky="nsew")
 
-        window.title("Set conditions")
-        window.geometry("600x200")
+        submit_button = Button(top, text="Submit", command=self.submit_values)
+        submit_button.grid(row=2, column=1, columnspan=4, sticky="nsew")
 
-        btn_add_row = tk.Button(window, text="Add condition row", command=lambda m=window, c=cols: self.__add_conditions_row(window,cols))
-        btn_add_row.grid(row=1, column=1, columnspan=5)
+        self.__add_conditions_row(top,cols)
 
-        # submit_button = tk.Button(window, text="Submit", command=lambda s=stmts_select,c=cols_select,cd=condition_select,cv=cond_val_input,l=limit_input: self.__submit_query_conditions(s,c,cd,cv,l))
-        # submit_button.grid(row=2, column=1, columnspan=5)
-
-        window.mainloop()
+        top.mainloop()
     
     def __add_conditions_row(self,win,columns):
-
         rows_counter = self.__condition_rows_counter
 
-        if(rows_counter == 2):
+        if(rows_counter == 3):
             conditional_expressions = ["WHERE", "AND", "OR"]
         else:
             conditional_expressions = ["AND", "OR"]
         
-        selected_expr = tk.StringVar()
-        expr_select = ttk.Combobox(win, textvariable=selected_expr, values=conditional_expressions)
+        selected_expression  = StringVar()
+        expr_select = ttk.Combobox(win, textvariable=selected_expression, values=conditional_expressions)
         expr_select.grid(row=rows_counter,column=1,sticky="nsew")
 
-        selected_col = tk.StringVar()
-        columns_select = ttk.Combobox(win,textvariable=selected_col,values=columns)
-        columns_select.grid(row=rows_counter,column=2,sticky="nsew")
+        selected_col = StringVar()
+        column_select = ttk.Combobox(win,textvariable=selected_col,values=columns)
+        column_select.grid(row=rows_counter,column=2,sticky="nsew")
 
         operators = ["=", ">", "<", ">=", "<=", "LIKE","NOT LIKE"]
-        selected_operator = tk.StringVar()
+        selected_operator = StringVar()
         operators_select = ttk.Combobox(win, textvariable=selected_operator, values=operators)
         operators_select.grid(row=rows_counter,column=3,sticky="nsew")
-        
-        condition_val_input = tk.Entry(win)
+
+        condition_val_input = Entry(win)
         condition_val_input.grid(row=rows_counter,column=4,sticky="nsew")
 
-        limit_input = tk.Entry(win)
-        limit_input.grid(row=rows_counter,column=5,sticky="nsew")
-
+        self.__query_conditions.append({
+            'selected_expr': selected_expression,
+            'selected_col': selected_col,
+            'selected_operator': selected_operator,
+            'condition_val_input': condition_val_input
+        })
+               
         self.__condition_rows_counter += 1
 
+    def submit_values(self):
+        for conditions in self.__query_conditions:
 
+            expr = conditions['selected_expr'].get()
+            col = conditions['selected_col'].get()
+            oper = conditions['selected_operator'].get()
+            val = conditions['condition_val_input'].get()
 
-    # def __submit_query_conditions(self,stmt,col,cond,val,limit):
-    #     selected_condition_stmt = stmt.get()
-    #     selected_column = col.get()
-    #     selected_cond = cond.get()
-    #     condition_val = val.get()
-    #     query_limit = limit.get()
+            if len(expr) and len(col) and len(oper) and len(val):
+                row_data = {
+                    "expression": conditions['selected_expr'].get(),
+                    "column": conditions['selected_col'].get(),
+                    "operator": conditions['selected_operator'].get(),
+                    "value": conditions['condition_val_input'].get()
+                }
+                print(row_data)
 
-    #     query_condition = f"{selected_condition_stmt} {selected_column} {selected_cond} {selected_cond} LIMIT {query_limit}"
-
-    #     condition_query = {"condition": query_condition}
-    #     print(condition_query)
-            
 
     def __prepare_export_query(self, columns, table):
         query = "SELECT "
@@ -269,7 +276,7 @@ class Export:
         return date
 
     def __apply_columns_style(self):
-        window = self.__tk_window
+        window = self.__tk_root_window
         window.columnconfigure(0, weight=1)
         window.columnconfigure(1, weight=1)
 
